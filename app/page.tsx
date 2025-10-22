7a38430228f3a69e701f6e2d9a1af58189365861
froyo-3 <emiltye@gmail.com>
2025-10-22 13:20:24 +0100

    dsfaf

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from "next/image";
 import Head from "next/head";
 import { useEffect } from "react";
@@ -20,6 +22,7 @@ export default function Home() {
       } else {
         const accessToken = await getAccessToken(clientId, code);
         const profile = await fetchProfile(accessToken);
+        console.log(profile);
         populateUI(profile);
       }
     }
